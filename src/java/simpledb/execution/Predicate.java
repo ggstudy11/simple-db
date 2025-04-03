175c15a742d708c5e7aa51fc3b1ee675ad7dff20
ggstudy11 <439403849@qq.com>
2025-04-03 15:39:21 +0800

    Predicate completed

--- src/java/simpledb/execution/Predicate.java
+++ src/java/simpledb/execution/Predicate.java
@@ -47,6 +47,9 @@ public class Predicate implements Serializable {
         }
 
     }
+    private final int field;
+    private final Op op;
+    private final Field operand;
 
     /**
      * Constructor.
@@ -56,31 +59,30 @@ public class Predicate implements Serializable {
      * @param operand field value to compare passed in tuples to
      */
     public Predicate(int field, Op op, Field operand) {
-        // TODO: some code goes here
+        this.field = field;
+        this.op = op;
+        this.operand = operand;
     }
 
     /**
      * @return the field number
      */
     public int getField() {
-        // TODO: some code goes here
-        return -1;
+        return field;
     }
 
     /**
      * @return the operator
      */
     public Op getOp() {
-        // TODO: some code goes here
-        return null;
+        return op;
     }
 
     /**
      * @return the operand
      */
     public Field getOperand() {
-        // TODO: some code goes here
-        return null;
+        return operand;
     }
 
     /**
@@ -93,8 +95,7 @@ public class Predicate implements Serializable {
      * @return true if the comparison is true, false otherwise.
      */
     public boolean filter(Tuple t) {
-        // TODO: some code goes here
-        return false;
+        return t.getField(field).compare(op, operand);
     }
 
     /**
@@ -102,7 +103,10 @@ public class Predicate implements Serializable {
      * operand_string"
      */
     public String toString() {
-        // TODO: some code goes here
-        return "";
+        StringBuilder sb = new StringBuilder();
+        sb.append("f = " + field);
+        sb.append(" op = " + op.toString());
+        sb.append(" operand = " + operand.toString());
+        return sb.toString();
     }
 }
